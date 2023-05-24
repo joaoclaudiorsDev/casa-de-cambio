1ec920d695f2f848c8ed494d747ad7ade2f2feae
João Cláudio <joaoclaudiobio@gmail.com>
2023-05-24 14:51:14 -0300

    importando api

---
+++ src/components.js
@@ -0,0 +1,29 @@
+
+
+const ulELement = document.querySelector('.container main ul')
+const h2ELement = document.querySelector('.container main h2')
+
+function createLiELement (name,value) {
+    const liELement = document.createElement('li');
+    liELement.innerHTML= `
+    <b>${name}</b>
+    <span>${value}</span>
+    `
+    return liELement;
+    // <li>
+    //     <b>USD</b>
+    //     <span>5.00</span>
+    // </li>
+};
+
+export function renderCoins(coins, baseCoin){
+  ulELement.innerHTML = '';
+  h2ELement.innerHTML = `Valores referentes a 1 ${baseCoin}`;
+
+  coins.forEach(element => {
+    const name = element.name;
+    const value = element.value;
+    const liELement = createLiELement(name, value)
+    ulELement.appendChild(liELement)
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,2 +1,25 @@
 import './style.css'
 import './reset.style.css'
+import { renderCoins } from './components'
+
+const inputElement = document.querySelector('header form input')
+const buttonElement = document.querySelector('header form button')
+
+const prototype = [
+    { name: 'USD', value: '5.00'},
+    { name: 'USD', value: '5.00'},
+    { name: 'USD', value: '5.00'},
+    { name: 'USD', value: '5.00'},
+    { name: 'USD', value: '5.00'},
+]
+
+renderCoins(prototype, 'BRL');
+
+buttonElement.addEventListener('click', () => {
+    const inputValue = inputElement.value;
+    fetch(`https://api.exchangerate.host/latest?base=${inputValue}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+})
